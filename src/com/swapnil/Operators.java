a0035f6ea124a454a3421e3fafea2d503193068a
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-18 19:40:39 +0530

    Operators in java

---
+++ src/com/swapnil/Operators.java
@@ -0,0 +1,18 @@
+package com.swapnil;
+
+import java.util.Scanner;
+
+public class Operators {
+    public  static void main(String[] args)
+    {
+        int a;
+        byte b=8;
+        Scanner in = new Scanner(System.in);
+        a= in.nextInt();
+        a+=2;
+        b*=2.5;
+        System.out.println("a increamented by 2 is:"+a);
+        System.out.println(b);
+    }
+}
+
