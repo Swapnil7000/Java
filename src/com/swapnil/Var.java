4f3014a62554fc99cd63060e19f54ca47f5fcef9
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-18 19:34:42 +0530

    Variables in java

---
+++ src/com/swapnil/Var.java
@@ -0,0 +1,15 @@
+package com.swapnil;
+
+public class Var {
+    public static void main(String[]args)
+    {
+        int x=12;
+        float f=12.12f;//Typecasted it into float by default double
+        double d=12.345;
+        boolean b=false;
+        System.out.println("Int is "+x);
+        System.out.println("Float value is "+f);
+        System.out.println("Double value is "+d);
+        System.out.println("Boolean value is "+b);
+    }
+}
