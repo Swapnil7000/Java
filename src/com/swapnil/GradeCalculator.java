9d9457310bacd90a35002ad30c8ccfdc237ca4aa
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-18 23:37:17 +0530

    To calculate grade of ju students

---
+++ src/com/swapnil/GradeCalculator.java
@@ -0,0 +1,25 @@
+package com.swapnil;
+
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        double insem1, insem2, attendance, endsem, assignment, total;
+        System.out.println("Enter the marks of insem1,insem2,endsem,assignment,attendance respectively");
+        Scanner gpa = new Scanner(System.in);
+        insem1 = gpa.nextDouble();
+        insem2 = gpa.nextDouble();
+        endsem = gpa.nextDouble();
+        assignment = gpa.nextDouble();
+        attendance = gpa.nextDouble();
+        total = 0.3 * insem1 + 0.3 * insem2 + attendance + assignment + 0.5 * endsem;
+        if (total >= 35) {
+
+            System.out.println("You crossed the danger zone by " + (total - 35));
+        }
+        else {
+            System.out.println("You missed the exam by " + (35-total));
+        }
+
+    }
+}
