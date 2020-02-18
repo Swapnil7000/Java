3f16b4ce4ba4be0c7fe592e02f6bc82bd6b4ce70
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-18 21:26:12 +0530

    To print sum of two numbers

---
+++ src/com/swapnil/MySum.java
@@ -0,0 +1,14 @@
+package com.swapnil;
+
+import java.util.Scanner;
+
+public class MySum {
+    public static void main(String[]args){
+        int a,b;
+        Scanner mySum = new Scanner(System.in);
+        System.out.println("Enter the two numbers");
+        a = mySum.nextInt();
+        b=mySum.nextInt();
+        System.out.println("Sum is "+(a+b));
+    }
+}
