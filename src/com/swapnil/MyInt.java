cc99f7d0c118b0f237c5db538e06b23871e99748
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-18 19:42:32 +0530

    To check that can u vote

---
+++ src/com/swapnil/MyInt.java
@@ -0,0 +1,21 @@
+package com.swapnil;
+
+import java.util.Scanner;
+
+public class MyInt {
+    public static void main(String[] args)
+    {
+        Scanner myAge=new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int Myage=myAge.nextInt();//To take input from the user
+        if(Myage>=18)
+        {
+            System.out.println("You can vote");
+
+        }
+        else
+        {
+            System.out.println("You cannot vote");
+        }
+    }
+}
