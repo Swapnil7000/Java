31d5f45b0c6edd54acc007fdc9e2f36fdb5e370f
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-18 19:41:33 +0530

    To print the entered string

---
+++ src/com/swapnil/MyStr.java
@@ -0,0 +1,13 @@
+package com.swapnil;
+
+import java.util.Scanner;
+
+public class MyStr {
+    public static void main(String[] args)
+    {   String str="";
+        Scanner myName=new Scanner(System.in);
+        System.out.println("Enter the string:");
+        str=myName.nextLine();
+        System.out.println("Your name is "+str);
+    }
+}
