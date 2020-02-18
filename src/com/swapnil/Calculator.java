74b440dadb24e12905767e81f6078c67a589a9df
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-18 22:25:16 +0530

    To calculate the general operations

---
+++ src/com/swapnil/Calculator.java
@@ -0,0 +1,33 @@
+package com.swapnil;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[]args) {
+        double a, b;
+        char ch;
+        Scanner calci = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        a = calci.nextFloat();
+        System.out.println("Enter the operation +,-,*,/,%");
+        ch = calci.next().charAt(0);
+        System.out.println("Enter the second number");
+        b = calci.nextFloat();
+        switch (ch)
+        {
+            case '+': System.out.println("Sum is "+(a+b));
+            break;
+            case '-': System.out.println("Subtraction is "+(a-b));
+            break;
+            case '*': System.out.println("Multiplication is "+(a*b));
+            break;
+            case '/': System.out.println("Divison is "+(a/b));
+            break;
+            case '%': System.out.println("Remainder on dividing first number by second is "+(a%b));
+            break;
+            default: System.out.println("Invalid operation");
+            break;
+        }
+
+    }
+}
