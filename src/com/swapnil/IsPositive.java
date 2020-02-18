754219243aee53dc97533fe7e7d579a5689f2d11
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-18 19:46:30 +0530

    To check positive,zero,negative

---
+++ src/com/swapnil/IsPositive.java
@@ -0,0 +1,24 @@
+package com.swapnil;
+
+import java.util.Scanner;
+
+public class IsPositive {
+    public static void main(String[]args)
+    {
+        Scanner no= new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int i=no.nextInt();
+        if(i>0)
+        {
+            System.out.println("Number is positive");
+        }
+        else if(i==0)
+        {
+            System.out.println("Number is zero");
+        }
+        else
+        {
+            System.out.println("Number is negative");
+        }
+    }
+}
