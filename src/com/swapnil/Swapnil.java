2f7e7e7f3af3e814b14b6940874fe10e1bbafada
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-18 19:39:47 +0530

    To print a message

---
+++ src/com/swapnil/Swapnil.java
@@ -0,0 +1,12 @@
+package com.swapnil;
+
+public class Swapnil {
+    public static void main(String[] args)
+    {
+        SayHi();
+    }
+    public static void SayHi(){
+        System.out.println("Hi from swapnil");
+    }
+
+}
