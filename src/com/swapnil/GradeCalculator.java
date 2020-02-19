0c0be147be2c209e6559e5eea900cb7219239f67
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-19 22:48:19 +0530

    Now,showing the scored marks

--- src/com/swapnil/GradeCalculator.java
+++ src/com/swapnil/GradeCalculator.java
@@ -15,11 +15,12 @@ public class GradeCalculator {
         total = 0.3 * insem1 + 0.3 * insem2 + attendance + assignment + 0.5 * endsem;
         if (total >= 35) {
 
-            System.out.println("You crossed the danger zone by " + (total - 35));
+            System.out.println("You crossed the danger zone by " + (total - 35)+" marks");
         }
         else {
-            System.out.println("You missed the exam by " + (35-total));
+            System.out.println("You missed the exam by " + (35-total)+" marks");
         }
+        System.out.println("You scored "+(total)+" marks");
 
     }
 }
