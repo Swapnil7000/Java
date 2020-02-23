d8e18666d838d6c745d38cfa1866b2809a4c93ae
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-23 19:47:46 +0530

    To print all array elements using for loop

---
+++ src/com/swapnil/Array2.java
@@ -0,0 +1,11 @@
+package com.swapnil;
+class Array2{
+    public static void main(String[]args) {
+        int[] Myarray={12,34,56,77,91};
+        System.out.println("Array elements are ");
+        for(int index=0;index<Myarray.length;index++)
+        {
+            System.out.println(Myarray[index]);
+        }
+    }
+}
