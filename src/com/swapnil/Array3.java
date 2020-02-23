fcd416a225cc36d57f6fef46789348479658511b
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-23 20:06:29 +0530

    To print array length and all strings in it

---
+++ src/com/swapnil/Array3.java
@@ -0,0 +1,10 @@
+package com.swapnil;
+class Array3{
+    public static void main(String[] args) {
+        String []myNames={"Swapnil","Shubham","Ballo","Ek number"};
+        System.out.println(myNames.length);
+        for (String string : myNames) {
+            System.out.println(string);
+        }
+    }
+}
