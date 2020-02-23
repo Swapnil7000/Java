0a5bc02e755eea5c5770625f45319795915e7904
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-23 13:06:18 +0530

    To print the triangular pattern

---
+++ src/com/swapnil/StarPattern3.java
@@ -0,0 +1,33 @@
+// To print the star pattern like this
+/* Enter the number of rows:
+5
+      *
+     * *
+    * * *
+   * * * *
+  * * * * *   */
+package com.swapnil;
+
+import java.util.Scanner;
+class StarPattern3{
+     public static void main(String[] args) {
+        int r,i,j,l;
+        Scanner star=new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        r=star.nextInt();
+        for(i=0;i<r;i++)
+        {
+            for(l=0;l<=r-i;l++)
+            {
+                System.out.print(" ");
+            }
+            for(j=0;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
