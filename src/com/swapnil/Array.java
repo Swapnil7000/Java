9f775e02cbef380a27105e56037f4c451e449539
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-23 19:30:36 +0530

    To print an array element

---
+++ src/com/swapnil/Array.java
@@ -0,0 +1,11 @@
+package com.swapnil;
+
+class Array{
+
+    public static void main(String[]args) {
+        int []myArray={1,2,3,4};
+        // int myArray[]={1,2,3,4}; Not preferrable
+        System.out.println(myArray[3]);
+        
+    }
+}
