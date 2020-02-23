3858a32385748b286a0c9222a1568e5022d6695d
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-23 19:40:39 +0530

    To print all array elements

---
+++ src/com/swapnil/Array1.java
@@ -0,0 +1,15 @@
+package com.swapnil;
+class Array1{
+    public static void main(String[]args) {
+        int []myArray1={12,23,34,45,56};
+        int i=0;
+        System.out.println("Array elements are: ");
+        while(i<myArray1.length)
+        {
+            System.out.println(myArray1[i]);
+            i++;
+        }
+
+        
+    }
+}
