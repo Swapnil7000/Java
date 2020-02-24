eeb9514ba47374b059cb8ad38c31cabc678790e7
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-24 18:20:02 +0530

    Strings in java

---
+++ src/com/swapnil/String1.java
@@ -0,0 +1,12 @@
+package com.swapnil;
+
+class String1 {
+    public static void main(String[] args) {
+        String myString = "Swapnil bhojwani";
+        System.out.println("Your string is " + myString);
+        System.out.println("Your string length is " + myString.length());
+        System.out.println("Your string in lower case is " + myString.toLowerCase());
+        System.out.println("Your string in upper case is " + myString.toUpperCase());
+
+    }
+}
