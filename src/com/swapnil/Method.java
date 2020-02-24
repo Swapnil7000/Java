b03e753d60165d1a6d1773766fdf23cae65897bd
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-24 18:33:56 +0530

    Method created to print message

---
+++ src/com/swapnil/Method.java
@@ -0,0 +1,14 @@
+package com.swapnil;
+
+class Method {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            name();
+        }
+
+    }
+
+    public static void name() {
+        System.out.println("Hello guys");
+    }
+}
