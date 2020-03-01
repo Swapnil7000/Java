03adfe58debdafd14d91bcba150b5b8985e3c978
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-03-01 13:40:51 +0530

    To calculate simple interest of given amount

---
+++ src/com/swapnil/Interest.java
@@ -0,0 +1,15 @@
+package com.swapnil;
+
+import java.util.Scanner;
+
+class Interest {
+    public static void main(String[] args) {
+        System.out.println("Enter the value of principle amount,rate of interest,time");
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        float r = sc.nextFloat();
+        int t = sc.nextInt();
+        float Interest = p * r * t / 100;
+        System.out.println("Simple interest is " + Interest);
+    }
+}
